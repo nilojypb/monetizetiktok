766d6bca59497628593b8d88efc4432eb6db64f0
nilojypb <danilo.goncalves3@aluno.pb.gov.br>
2025-01-24 23:01:53 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './Login';  // Importando a página de login
+
+function App() {
+  return (
+    <div className="App">
+      <Login />  {/* Exibindo a página de login */}
+    </div>
+  );
+}
+
+export default App;
